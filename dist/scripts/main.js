78a24aece330e9979b4b310aa716df2a5f5aafb9
Alina Digtyar <alinadigtyar@gmail.com>
2016-12-15 19:06:43 +0200

    first trial of js

---
+++ dist/scripts/main.js
@@ -0,0 +1,12 @@
+;(function() {
+	"use strict";
+
+	var userAnswer = prompt("What is the official name of Javascript?");
+
+	if (userAnswer === "ECMAScript") {
+		console.log("Correct!");
+	} else {
+		console.log("Didn't you know? ECMAScript");
+	}
+
+})();
